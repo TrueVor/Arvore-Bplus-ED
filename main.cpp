2e126b98dd7bcba7d420c2ebc832c02537ef80b1
izabelle-tome <80071911+izabelle-tome@users.noreply.github.com>
2021-03-13 14:08:33 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
+#include <ctime> // para suportar a contagem de tempo na busca pelos elementos
 #include "arvoreBmais.hpp"
 
 using namespace std;
 
 int main() {
     sequenceset meuSeqSet("teste.dat");
+    arvoreB bMais;
     dado umDado;
     tipoChave umaChave;
     char operacao;
+
+    clock_t buscaSequenceSet;
+    clock_t buscaBMais;
 
     do {
         try {
@@ -19,8 +24,22 @@ int main() {
                     break;
                 case 'b': // buscar
                     cin >> umaChave;
+
+                    /*buscaSequenceSet = clock();*/
                     umDado = meuSeqSet.buscar(umaChave);
+                    //buscaSequenceSet = (clock() - buscaSequenceSet);
+
+                    /*buscaBMais = clock();
+                    umDado = bMais.buscar(umaChave);
+                    buscaBMais = (clock() - buscaBMais);*/
+
                     cout << "Busca: "<< umDado.chave << "/" << umDado.valor << endl;
+
+                    /*cout << endl;
+                    cout << "SequenceSet X B+" << endl;
+                    cout << "O tempo de busca do elemento no SequenceSet foi de: " << buscaSequenceSet << " milissegundos!" << endl;
+                    cout << "O tempo de busca do elemento na B+ foi de: " << buscaBMais << " milissegundos!" << endl;*/
+                    
                     break;
                 case 'p': // mostrar estrutura
                     meuSeqSet.imprimir();
